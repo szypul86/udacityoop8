fe266dd0cd2cd7b92961e6aace1aeec5d653bbc0
Andrzej Szypulski <szypul86@gmail.com>
2019-07-03 11:37:35 +0200

    main method added with connecting instances

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,5 +1,29 @@
 public class BankAccount {
-    String account;
-    double balance;
+
+    String account = "wfbodsjbfodsj";
+    double balance = -1234.9;
+
+
+
+    public static void main(String [] args){
+
+
+        BankAccount bankAccount = new BankAccount();
+        CheckingAccount checkingAccount = new CheckingAccount();
+        SavingsAccount savingsAccount = new SavingsAccount();
+        COD cod = new COD();
+
+
+        savingsAccount.transfers = 5;
+        savingsAccount.balance = bankAccount.balance + 200;
+
+
+        System.out.println(savingsAccount.transfers);
+        System.out.println(savingsAccount.account);
+        System.out.println(savingsAccount.balance);
+        System.out.println(bankAccount.balance);
+
+    }
+
 
 }
